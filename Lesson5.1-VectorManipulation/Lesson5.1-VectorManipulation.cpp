f9bdbf825ebcb968ffa936a0d995f851876f3079
Chris Prunotto <Chris.Prunotto@gmail.com>
2020-08-13 13:02:29 -0500

    Update Lesson5.1-VectorManipulation.cpp

--- Lesson5.1-VectorManipulation/Lesson5.1-VectorManipulation.cpp
+++ Lesson5.1-VectorManipulation/Lesson5.1-VectorManipulation.cpp
@@ -2,19 +2,31 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+
+	std::vector<int> vectorList = { 2, 4, 3, 6, 1, 9 };
+
+	int evenSum = 0;
+	int oddProduct = 1;
+
+	for (int i = 0; i < vectorList.size(); i++)
+	{
+		std::cout << "Current number: " << vectorList[i] << std::endl;
+		if (vectorList[i] % 2 == 0)
+		{
+			std::cout << "Even number! \nEven Sum running total: ";
+			evenSum = evenSum + vectorList[i];
+			std::cout << evenSum << std::endl << "\n";
+		}
+		else {
+			std::cout << "Odd number! \nOdd Product Running Total: ";
+			oddProduct = oddProduct * vectorList[i];
+			std::cout << oddProduct << std::endl << "\n";
+		}
+	}
+
+	std::cout << "\n------\nSum of even numbers in vector: " << evenSum << std::endl << "Product of odd numbers in vector: " << oddProduct << std::endl;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
